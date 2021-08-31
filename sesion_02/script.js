fe10502c2a7e69267c86a3e282c697d48a576640
Felipe Cortez <hola@faco.cl>
2021-08-30 21:51:51 -0400

    Add files via upload

--- sesion_02/script.js
+++ sesion_02/script.js
@@ -1,46 +1,49 @@
-var elEncabezado = document.querySelector("header");
-var laHora = new Date().getHours();
 var dondeEstas;
+
 if (document.body.classList.contains("portada")) {
     dondeEstas = true;
 } else {
     dondeEstas = false;
 }
+
+var laHora = new Date().getHours();
+
 var elSaludo;
+
 if (5 < laHora && laHora < 12) {
-    elSaludo = "Buenos días";
+    elSaludo = "Buenos días. ";
 } else if (11 < laHora && laHora < 21) {
-    elSaludo = "Buenas tardes";
+    elSaludo = "Buenas tardes. ";
 } else {
-    elSaludo = "Buenas noches";
+    elSaludo = "Buenas noches. ";
 }
-var elColor;
+
+var elEncabezado = document.querySelector("header");
+
+var loPrincipal = document.querySelector("main");
+
+var lorenIpsum = ["párrafo","texto","escrito","pasaje","enunciado"];
+
 function setup() {
-    createCanvas(windowWidth, windowHeight).position(0, 0).style("z-index", -1);
-    background(200);
+    noCanvas();
     createElement("h1", elSaludo).parent(elEncabezado).id("title");
     createA("index.html", "index").parent("vinculos");
     createA("page.html", "page").parent("vinculos");
-    elColor = createColorPicker("#555555").parent("controles");
-    elSlider = createSlider(1, 5, 3).parent("controles");
     if (dondeEstas) {
         portada();
     } else {
         pagina();
     }
 }
-function draw() {
-    stroke(elColor.color());
-    strokeWeight(elSlider.value());
-    if (mouseIsPressed) {
-        line(pmouseX, pmouseY, mouseX, mouseY);
-    }
-}
+
 function portada() {
-    createSpan(". Estás en index.html").parent("title");
+    createSpan("Estás en index.html").parent("title");
+    createElement('p','Y este es un '+random(lorenIpsum)+' en la portada.').parent(loPrincipal);
     select("a:nth-child(1)").style("font-weight", "bold").style("color", "#000000");
 }
+
 function pagina() {
-    createSpan(". Estás en page.html").parent("title");
+    createSpan("Estás en page.html").parent("title");
+    createElement('p','Y este es un '+random(lorenIpsum)+' en la página.').parent(loPrincipal);
     select("a:nth-child(2)").style("font-weight", "bold").style("color", "#000000");
 }
